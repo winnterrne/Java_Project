5081e78646feb66eb95f7794a267f67a02d4a064
Nguyễn Quang Huy <188671786+Dev-Huy@users.noreply.github.com>
2026-03-15 17:17:34 +0700

    merge DKha2 into all

--- Java_Project/src/main/java/BUS/SanPham_BUS.java
+++ Java_Project/src/main/java/BUS/SanPham_BUS.java
@@ -28,10 +28,13 @@ public class SanPham_BUS {
         }
         return spDAO.getAllSanPhamByMaDM(maDM);
     }
-
-    public ArrayList<SanPham_DTO> getAllTenSP() {
-        return spDAO.getAllTenSP();
-    }
+//    public ArrayList<SanPham_DTO> getAllTenSP() {
+//        return spDAO.getAllTenSP();
+//    }
+//
+//    public ArrayList<SanPham_DTO> getAllTenSP() {
+//        return spDAO.getAllTenSP();
+//    }
 
     public ArrayList<SanPham_DTO> layDsSanPhamConTon() {
         ArrayList<SanPham_DTO> list = new ArrayList<>();
